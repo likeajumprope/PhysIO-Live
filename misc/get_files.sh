2acc376c6107c16748148251cba8993d6ba092b1
Johanna Bayer <bayerj@student.unimelb.edu.au>
2023-05-05 13:53:21 +1000

    add get_files.sh

---
+++ misc/get_files.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+curl --create-dirs https://openneuro.org/crn/datasets/ds000113/objects/152ffaf7decf9a4c85a85f21a662a73218284a86 -o recording-cardresp_physio.json
+curl --create-dirs https://openneuro.org/crn/datasets/ds000113/objects/MD5E-s161017800--e3d1a133a2b66ddc0bfbb6941fa7ef14.nii.gz -o sub-01/ses-auditoryperception/func/sub-01_ses-auditoryperception_task-auditoryperception_run-01_bold.nii.gz
+curl --create-dirs https://openneuro.org/crn/datasets/ds000113/objects/MD5E-s239059--98b2cade77eda83c285ce215e67f1dd3.tsv.gz -o sub-01/ses-auditoryperception/func/sub-01_ses-auditoryperception_task-auditoryperception_run-01_physio.tsv.gz
+curl --create-dirs https://openneuro.org/crn/datasets/ds000113/objects/MD5E-s13932380--99d36b59ccefc9a6549209e62ed8d056.nii.gz -o sub-01/ses-forrestgump/anat/sub-01_ses-forrestgump_T1w.nii.gz
